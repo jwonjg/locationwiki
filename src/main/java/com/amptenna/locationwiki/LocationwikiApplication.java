7338475263542fc92ec611a5617972f58cc544b4
jinwonjeong <jwonjg@gmail.com>
2019-03-30 16:05:13 +0900

    init

---
+++ src/main/java/com/amptenna/locationwiki/LocationwikiApplication.java
@@ -0,0 +1,13 @@
+package com.amptenna.locationwiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationwikiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocationwikiApplication.class, args);
+    }
+
+}
