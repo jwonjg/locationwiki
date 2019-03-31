f1c1d71da4acd003b492cc7eb8d82d032346d73d
jinwonjeong <jwonjg@gmail.com>
2019-03-31 16:09:29 +0900

    health

--- src/main/java/com/amptenna/locationwiki/HelloController.java
+++ src/main/java/com/amptenna/locationwiki/HelloController.java
@@ -1,5 +1,6 @@
 package com.amptenna.locationwiki;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,5 +11,10 @@ public class HelloController {
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello() {
         return "hello";
+    }
+
+    @RequestMapping(value = "/health", method = RequestMethod.GET)
+    public ResponseEntity health() {
+        return ResponseEntity.ok().build();
     }
 }
